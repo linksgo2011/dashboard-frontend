e04c1a7f03e890916c15820b64c1d6cf92d4eaa2
Ning <120377843>
2018-12-03 22:11:43 +0800

    new featuers

---
+++ src/components/navigator/Navigator.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="navigator" id="navigator">
+    <ul>
+      <li><a href="">常用导航</a></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Navigator"
+};
+</script>
---
+++ src/components/notes/Notes.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="notes" id="notes">
+    <textarea class="stick" cols="30" rows="10" placeholder="You can put your notes here temporarily!"></textarea>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Notes"
+};
+</script>
+<style scoped>
+@import "Notes.less";
+</style>
